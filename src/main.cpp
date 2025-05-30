ba8da3aac560af7ba3b35ff18f77dd069560577d
EmmanuelOZ <davidozuna84@hotmail.com>
2025-05-30 00:05:06 -0600

    fix(connection) recibir datos del wifi y conectarme a la red

--- src/main.cpp
+++ src/main.cpp
@@ -2,60 +2,120 @@
 #include "SerialComm.h"
 #include <ArduinoJson.h>
 #include <Socket.h>
+#include <WiFi.h>
 
 SerialComm comm(Serial1);
-String state = "repose";
+
+String ssid = "";
+String password = "";
+String apiKey = "";
 
 void setup()
 {
   Serial.begin(9600);
-  comm.begin(9600, 134217756U, 16, 17);
+  comm.begin(9600, 134217756U, 16, 17); // Serial1, RX=16, TX=17
   Serial.println("Setup");
 }
 
-void loop()
+void connectInit(Keyvalue kv[], int count)
 {
-  Keyvalue kv[MAX_PAIRS];
-  int count = comm.receive(kv);
+  for (int i = 0; i < count; i++)
+  {
+    if (kv[i].key == "apiKey")
+    {
+      apiKey = kv[i].value;
+    }
+    else if (kv[i].key == "ssid")
+    {
+      ssid = kv[i].value;
+    }
+    else if (kv[i].key == "password")
+    {
+      password = kv[i].value;
+    }
+  }
+
+  WiFi.begin(ssid.c_str(), password.c_str());
+  Serial.print("Connecting to WiFi: ");
+  Serial.println(ssid);
+  Serial.println(password);
+
+  if (WiFi.status() == WL_CONNECT_FAILED)
+  {
+    Serial.println("Connection failed, check SSID and password");
+    comm.send("response=connectFailed");
+    return;
+  }
+  else if (WiFi.status() == WL_NO_SSID_AVAIL)
+  {
+    Serial.println("No SSID available, check SSID");
+    comm.send("response=connectFailed");
+    return;
+  }
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    comm.send("response=connecting");
+  }
+  Serial.println("Connected to WiFi");
+  comm.send("response=connectSuccess");
+}
+
+void seenRecords(Keyvalue kv[], int count)
+{
   JsonDocument doc = JsonDocument();
   doc["color"] = JsonObject();
 
   for (int i = 0; i < count; i++)
   {
 
-    if (kv[i].key == "state")
+    if (kv[i].key == "colorR")
     {
-      state = kv[i].value;
-      doc["state"] = kv[i].value;
+      doc["color"]["r"] = kv[i].value;
     }
-
-    if (state == "reading")
+    else if (kv[i].key == "colorG")
     {
-      if (kv[i].key == "colorR")
-      {
-        doc["color"]["r"] = kv[i].value;
-      }
-      else if (kv[i].key == "colorG")
-      {
-        doc["color"]["g"] = kv[i].value;
-      }
-      else if (kv[i].key == "colorB")
-      {
-        doc["color"]["b"] = kv[i].value;
-      }
-      else
-      {
-        doc[kv[i].key] = kv[i].value;
-      }
+      doc["color"]["g"] = kv[i].value;
+    }
+    else if (kv[i].key == "colorB")
+    {
+      doc["color"]["b"] = kv[i].value;
+    }
+    else
+    {
+      doc[kv[i].key] = kv[i].value;
     }
   }
+  Socket::send(doc);
+  comm.send("response=seenSuccess");
+}
 
-  // Si el hay valores en el kv
-  if (count > 0 && state == "reading")
+void loop()
+{
+  Keyvalue kv[MAX_PAIRS];
+  int count = comm.receive(kv);
+
+  if (count > 0 && kv[0].key == "command")
   {
-    Socket::send(doc);
-    comm.send("ok");
+    if (kv[0].value == "connectInit")
+    {
+      connectInit(kv, count);
+    }
+    else if (kv[0].value == "seen")
+    {
+      seenRecords(kv, count);
+    }
+    else if (kv[0].value == "seenFinished")
+    {
+      Serial.println("Seen finished");
+    }
+    else
+    {
+      Serial.print("Unknown command: ");
+      Serial.println(kv[0].key);
+      comm.send("response=UnknownCommand");
+    }
   }
-
-  delay(1000);
 }
